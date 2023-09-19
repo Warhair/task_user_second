4c8685eb37735e238546c6e3443a3e4f87eff766
Warhair <tegzesmarton@gmail.com>
2023-09-19 15:44:37 +0200

    oraifeladat

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,32 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/api/tasks', [TaskController::class, 'index']);
+Route::get('/api/tasks/{id}', [TaskController::class, 'show']);
+Route::post('/api/tasks', [TaskController::class, 'store']);
+Route::put('/api/tasks/{id}', [TaskController::class, 'update']);
+
+Route::delete('/api/tasks/{id}', [TaskController::class, 'destroy']);
+
+Route::get('/api/users', [UserController::class, 'index']);
+
+Route::get('/task/new', [TaskController::class, 'newView']);
+
+Route::get('/task/edit/{id}', [TaskController::class, 'editView']);
+Route::get('/task/list', [TaskController::class, 'listView']);
+
+Route::get('/api/users', [UserController::class, 'index']);
+Route::get('/api/users/{id}', [UserController::class, 'show']);
+Route::post('/api/users', [UserController::class, 'store']);
+Route::put('/api/users/{id}', [UserController::class, 'update']);
+
+Route::delete('/api/users/{id}', [UserController::class, 'destroy']);
+
+Route::get('/api/users', [UserController::class, 'index']);
+
+Route::get('/task/new', [UserController::class, 'newView']);
+
+Route::get('/task/edit/{id}', [UserController::class, 'editView']);
+Route::get('/task/list', [UserController::class, 'listView']);
 
 //require __DIR__ . '/auth.php';
